f4999ebebe1e0a6578ba0ea1111f79cd988679a5
Julia Shypat <yuliia.shypat@algoteque.com>
2019-11-21 17:24:44 +0100

    Preparation

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,49 @@
 const painters = [
   {
-    firstName: "Herb",
-    lastName: "Aach",
+    firstName: 'Herb',
+    lastName: 'Aach',
     born: 1923,
     passed: 1985,
     paintings: 12
   },
   {
-    firstName: "Pacita",
-    lastName: "Abad",
+    firstName: 'Pacita',
+    lastName: 'Abad',
     born: 1946,
     passed: 2004,
     paintings: 52
   },
   {
-    firstName: "Daniel",
-    lastName: "Maclise",
+    firstName: 'Daniel',
+    lastName: 'Maclise',
     born: 1806,
     passed: 1870,
     paintings: 2
   },
   {
-    firstName: "Aileen",
-    lastName: "Eagleton",
+    firstName: 'Aileen',
+    lastName: 'Eagleton',
     born: 1902,
     passed: 1984,
     paintings: 35
   },
   {
-    firstName: "Thomas",
-    lastName: "Eakins",
+    firstName: 'Thomas',
+    lastName: 'Eakins',
     born: 1844,
     passed: 1916,
     paintings: 1
   },
   {
-    firstName: "Edgar",
-    lastName: "Degas ",
+    firstName: 'Edgar',
+    lastName: 'Degas ',
     born: 1834,
     passed: 1917,
     paintings: 34
   },
   {
-    firstName: "Martin",
-    lastName: "Desjardins",
+    firstName: 'Martin',
+    lastName: 'Desjardins',
     born: 1637,
     passed: 1694,
     paintings: 7
@@ -51,57 +51,57 @@ const painters = [
 ];
 
 const clients = [
-  "Fiona, Bownd",
-  "Bren, Strutt",
-  "Cletis, Cobelli",
-  "Booth, Kedge",
-  "Demetris, Parman",
-  "Cull, Itzhaki",
-  "Ertha, Bikker",
-  "Maggi, Stockport",
-  "Horace, Hearthfield",
-  "Vassili, Pomfret",
-  "Cirilo, Zottoli",
-  "Trueman, MacDermot",
-  "Karla, Spencer",
-  "Allys, McManamon",
-  "Saloma, Boolsen",
-  "Giacomo, Vedntyev",
-  "Essa, Blacket",
-  "Dari, Muncer",
-  "Jobi, Joscelyn",
-  "Eimile, Joberne"
+  'Fiona, Bownd',
+  'Bren, Strutt',
+  'Cletis, Cobelli',
+  'Booth, Kedge',
+  'Demetris, Parman',
+  'Cull, Itzhaki',
+  'Ertha, Bikker',
+  'Maggi, Stockport',
+  'Horace, Hearthfield',
+  'Vassili, Pomfret',
+  'Cirilo, Zottoli',
+  'Trueman, MacDermot',
+  'Karla, Spencer',
+  'Allys, McManamon',
+  'Saloma, Boolsen',
+  'Giacomo, Vedntyev',
+  'Essa, Blacket',
+  'Dari, Muncer',
+  'Jobi, Joscelyn',
+  'Eimile, Joberne'
 ];
 
 const cars = [
-  "Honda",
-  "Saab",
-  "Ford",
-  "Mitsubishi",
-  "Buick",
-  "Toyota",
-  "Mitsubishi",
-  "Mercedes-Ben",
-  "Buick",
-  "Volvo",
-  "Oldsmobile",
-  "Mercedes-Ben",
-  "Honda",
-  "Chevrolet",
-  "Volkswagen",
-  "GMC",
-  "Chevrolet",
-  "Jeep",
-  "Acura",
-  "Honda",
-  "Acura",
-  "Suzuki"
+  'Honda',
+  'Saab',
+  'Ford',
+  'Mitsubishi',
+  'Buick',
+  'Toyota',
+  'Mitsubishi',
+  'Mercedes-Ben',
+  'Buick',
+  'Volvo',
+  'Oldsmobile',
+  'Mercedes-Ben',
+  'Honda',
+  'Chevrolet',
+  'Volkswagen',
+  'GMC',
+  'Chevrolet',
+  'Jeep',
+  'Acura',
+  'Honda',
+  'Acura',
+  'Suzuki'
 ];
 console.table(painters);
 // 1. Array Length
-console.log("painters length", painters.length);
-console.log("clients length", clients.length);
-console.log("cars length", cars.length);
+console.log('painters length', painters.length);
+console.log('clients length', clients.length);
+console.log('cars length', cars.length);
 
 // 2. Array Iteration with FOR & foreach
 
@@ -151,8 +151,8 @@ console.table(sortedPainters);
 
 // Sort client by lastName
 const sortedClients = clients.sort((lastClient, nextClient) => {
-  const [lastClientFirstName, lastClientLastName] = lastClient.split(", ");
-  const [nextClientFirstName, nextClientLastName] = nextClient.split(", ");
+  const [lastClientFirstName, lastClientLastName] = lastClient.split(', ');
+  const [nextClientFirstName, nextClientLastName] = nextClient.split(', ');
   return lastClientLastName > nextClientLastName ? 1 : -1;
 });
 console.table(sortedClients);
@@ -163,7 +163,7 @@ console.table(sortedClients);
 const totalPaintings = painters.reduce((amount, painter) => {
   return amount + painter.paintings;
 }, 0);
-console.log("Total paintings: ", totalPaintings);
+console.log('Total paintings: ', totalPaintings);
 
 // Sum app cars
 
