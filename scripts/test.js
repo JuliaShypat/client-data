28ab83ab6721b7e224577b02f0a519cae88faa35
Julia Shypat <juliashipat@gmail.com>
2019-12-02 22:06:18 +0100

    Prep for login

--- scripts/test.js
+++ scripts/test.js
@@ -1,11 +1,36 @@
-let test;
-console.log(test);
+const link = document.querySelector(".profrontendLink");
+// console.log(link);
 
-let test2 = null;
-console.log(test2);
+link.addEventListener("click", event => {
+  const wantToContinue = confirm("Do you want to leave this page?");
 
-let test3 = NaN;
-console.log(test3);
+  if (!wantToContinue) {
+    event.preventDefault();
+  }
+});
 
-console.log(test === test2);
-console.log(test2 === null);
+const form = document.querySelector("[name='registerForm']");
+
+form.querySelector("[type='password']").addEventListener("blur", event => {
+  console.log(event.target);
+  validatePassword(event.target);
+});
+
+console.log(form);
+form.addEventListener("submit", event => {
+  event.preventDefault();
+  validateForm(event.target);
+});
+
+function validateForm(target) {
+  validatePassword(target.pass);
+}
+
+function validatePassword(field) {
+  if (field.value.length < 10) {
+    field.className += " is-invalid";
+    console.warn("Your pass is too weak!");
+  } else {
+    field.className = "form-control is-valid";
+  }
+}
